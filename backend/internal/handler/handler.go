4dd85ac831bbdba7ef9b241e70e69be1217be08d
tora <bmvdsfy@gmail.com>
2025-11-10 19:26:39 +0900

    型アサーション

--- backend/internal/handler/handler.go
+++ backend/internal/handler/handler.go
@@ -1,11 +1,18 @@
 package handler
 
+import (
+	"backend/pkg/schema"
+)
+
 // APIHandler combines all handlers to implement ServerInterface
 type APIHandler struct {
 	*UserHandler
 	*QuestionHandler
 	*FileHandler
 }
+
+// Ensure APIHandler implements schema.ServerInterface
+var _ schema.ServerInterface = APIHandler{}
 
 func NewAPIHandler() *APIHandler {
 	return &APIHandler{
